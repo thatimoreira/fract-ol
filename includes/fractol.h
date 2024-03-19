ffa041013a0e6fcbb68f42463b13e42ad5f23e44
thatimoreira <thatimoreira1@gmail.com>
2024-03-18 23:40:04 -0300

    Added auxiliary functions prototypes used to validate the user input

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 20:29:38 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/03/16 20:29:40 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/03/18 22:56:52 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,30 @@
 #include "libft/libft.h"
 #include <math.h>
 
+# define SIZE 720
+# define INVALID_USAGE "Too much or missing parameter(s)!" // Choose a better string
+# define INVALID_FRACTAL "Invalid fractal type!" // Choose a better string
+
+# define WIDTH 1000 // squares keep the rendering math simple
+# define HEIGHT 1000
+
 typedef struct s_fract // s_data
 {
-    void    *img;
-    char    *addr; // mem address frow where I'm going to change(???) the bytes
-    int     bpp; // bits per pixel
-    int     line_len;
-    int     endian; // used to specify how pixel data is stored or interpreted within an image (check thi case)
-}           t_fract;
+	char	*name;
+	int	user_option;
+	void	*img;
+	char	*addr; // mem address frow where I'm going to change(???) the bytes
+	int	bpp; // bits per pixel
+	int	line_len;
+	int	endian; // used to specify how pixel data is stored or interpreted within an image (check thi case)
+}		t_fract;
 
-void    pixel_put(t_fract fract, int x, int y, int color);
+void	pixel_put(t_fract fract, int x, int y, int color);
+t_fract	*validate_input(int n_args, char **args);
+void	handle_allocation_failure(void);
+void	render_mandelbrot(void *void_fractal);
+void	calculate_mandelbrot(t_fract *fractal);
+void	render_julia(void *void_fractal, double n1, double n2);
+void	calculate_julia(void *void_fractal, double n1, double n2);
 
 #endif
