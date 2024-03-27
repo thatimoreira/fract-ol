1c4752b848bb9d223f45e407e260b4900e11f68e
thatimoreira <thatimoreira.dev@gmail.com>
2024-03-27 06:12:40 -0300

    Renamed fractol.c to main.c and refactored it to improve readability and modularity. Separated input_valudation and fractal initialization into their respective funtions.

--- srcs/main.c
+++ srcs/main.c
@@ -6,35 +6,74 @@
 /*   By: tsoares- <tsoares-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 22:35:13 by tsoares-          #+#    #+#             */
-/*   Updated: 2024/03/18 22:37:13 by tsoares-         ###   ########.fr       */
+/*   Updated: 2024/03/27 06:09:01 by tsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 
-int	main(int argc, char **argv)
+void	set_fractal_name(t_fract *fractal, char **arguments)
 {
-	t_fract fractal;
-	int     user_option;
-
-	validate_input(argc, argv);
-
-	if (user_option == "1\0")
+	if (fractal->user_option == 1)
+		fractal->name = "Mandelbrot";
+	else if (fractal->user_option == 2)
 	{
-        	fractal.name == "Mandelbrot";
-		fractol_init(&fractal);
-		render_mandelbrot(&fractal);
+		fractal->name = "Julia";
+		fractal->zx = atof(arguments[2]);
+		fractal->zy = atof(arguments[3]);
+	}
+}
+
+int	validate_input(int n_args, char **args)
+{
+	int	user_op;
+
+	user_op = 0;
+	// ./fractol 1 => mandelbrot
+	// ./fractol 2 => julia
+	// ./fractol 2 <real> <i> => julia
+	if (n_args < 2)
+	{
+		ft_putstr_fd(INVALID_USAGE, STDERR_FILENO);
+		print_menu();
+		exit(EXIT_FAILURE);
+	}
+	user_op = ft_atoi(args[1]);
+	if (user_op != 1 && user_op!= 2)
+	{
+		ft_putstr_fd(INVALID_FRACTAL, STDERR_FILENO);
+		print_menu();
+		exit(EXIT_FAILURE);
+	}
+	else if ((user_op == 1 && n_args > 2) // Mandelbrot with params
+			|| (user_op == 2 && n_args != 4)) // Julia with less params then needed
+	{
+		ft_putstr_fd(INVALID_USAGE, STDERR_FILENO);
+		print_menu();
+		exit(EXIT_FAILURE);
 	}
 	else
+		return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_fract	*fractal;
+
+	fractal = (t_fract *)malloc(sizeof(t_fract));
+	if (!fractal)
+		handle_allocation_failure();
+	if (validate_input(argc, argv) == 0)
 	{
-		fractal.name == "Julia";
-		fractol_init(&fractal);
-		render_julia(&fractal); // complex numbers
+		fractal->user_option = ft_atoi(argv[1]);
+		set_fractal_name(fractal, argv);
+		fractol_init(fractal);
+		//init_minilibx(fractal);
+		render_fractals(fractal);
+		mlx_loop(fractal->mlx_connection);
+		free(fractal);
+		return (EXIT_SUCCESS);
 	}
-	mlx_loop(fractal.mlx_connection); // keeps the window open - mlx() function is used to receive events
-
-	// display_image(); -> displays the rendered img
-
-	// handle_user_input(); -> handle user input for zooming and navigation
-	return (0);
+	free(fractal);
+	return (EXIT_FAILURE);
 }
